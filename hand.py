2f3c54e775f9ba8f1e0fc3cf445bb2dd0e379821
Shogo Yamashita <s24c1130pf@s.chibakoudai.jp>
2025-11-03 14:05:56 +0900

    ROSの要素を削除

--- hand.py
+++ hand.py
@@ -4,29 +4,29 @@
 
 import cv2
 import mediapipe as mp
-import rospy
+#import rospy
 import math
 import sys
-from geometry_msgs.msg import Point
+#from geometry_msgs.msg import Point
 from numpy.lib.type_check import imag
-from robotdesign3_2021_1.msg import CustomArray
+#from robotdesign3_2021_1.msg import CustomArray
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 def main():
-  rospy.init_node("MediaPipe_node")
-  hand_position = rospy.Publisher('hand_topic', CustomArray, queue_size=10)
-  r = rospy.Rate(10) # 10hz
-  cap = cv2.VideoCapture(4)
-  array_points = CustomArray()
-  array_points.points= [0]
+  #rospy.init_node("MediaPipe_node")
+  #hand_position = rospy.Publisher('hand_topic', CustomArray, queue_size=10)
+  #r = rospy.Rate(10) # 10hz
+  cap = cv2.VideoCapture(0)
+  #array_points = CustomArray()
+  #array_points.points= [0]
   with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
 
     
-    while not rospy.is_shutdown():
+    while True: #not rospy.is_shutdown():
       
       success, image = cap.read()
       if not success:
@@ -53,6 +53,8 @@ def main():
                   cx4,cy4 = landmark_x, landmark_y
               if index == 8:
                   cx8,cy8 = landmark_x, landmark_y
+              if index == 9:
+                  cx9,cy9 = landmark_x, landmark_y
               if index == 12:
                   cx12,cy12 = landmark_x, landmark_y
               if index == 16:
@@ -61,13 +63,13 @@ def main():
                   cx20,cy20 = landmark_x, landmark_y
 
           #取得した座標より各指の中間点を表示
-          #gap1x,gap1y = (cx0+cx9)/2, (cy0+cy9)/2
-          #gap1x = int(gap1x)
-          #gap1y = int(gap1y)
-          #cv2.circle(image, (gap1x,gap1y), 5, (0, 255, 0), 2)
-          #gap1x,gap1y = gap1x-image_width/2,-(gap1y-image_height/2)
+          gap1x,gap1y = (cx0+cx9)/2, (cy0+cy9)/2
+          gap1x = int(gap1x)
+          gap1y = int(gap1y)
+          cv2.circle(image, (gap1x,gap1y), 5, (0, 255, 0), 2)
+          gap1x,gap1y = gap1x-image_width/2,-(gap1y-image_height/2)
 
-
+          """
           #取得した座標より各指の距離を表示
           gap1x,gap1y = cx0+cx4, cy0+cy4
           gap2x,gap2y = cx0+cx8, cy0+cy8
@@ -101,8 +103,8 @@ def main():
           gap3x,gap3y = gap3x-image_width/2,-(gap3y-image_height/2)
           gap4x,gap4y = gap4x-image_width/2,-(gap4y-image_height/2)
           gap5x,gap5y = gap5x-image_width/2,-(gap5y-image_height/2)
-
-
+          """
+          """
           array_points.points = []
 
           point_1 = Point()
@@ -134,25 +136,26 @@ def main():
 
           hand_position.publish(array_points)
           print(array_points.points[0].x)
+          """
           
           #カメラ画像に値を表示させたい場合は
           cv2.putText(image,"gap1x:"+str(gap1x)+"gap1y:"+str(gap1y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-          cv2.putText(image,"gap2x:"+str(gap2x)+"gap1y:"+str(gap2y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-          cv2.putText(image,"gap3x:"+str(gap3x)+"gap1y:"+str(gap3y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-          cv2.putText(image,"gap4x:"+str(gap4x)+"gap1y:"+str(gap4y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
-          cv2.putText(image,"gap5x:"+str(gap5x)+"gap1y:"+str(gap5y),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+         # cv2.putText(image,"gap2x:"+str(gap2x)+"gap1y:"+str(gap2y),(10,60),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+         # cv2.putText(image,"gap3x:"+str(gap3x)+"gap1y:"+str(gap3y),(10,90),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+         # cv2.putText(image,"gap4x:"+str(gap4x)+"gap1y:"+str(gap4y),(10,120),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+         # cv2.putText(image,"gap5x:"+str(gap5x)+"gap1y:"+str(gap5y),(10,150),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
 
 
           mp_drawing.draw_landmarks(
               image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
       cv2.imshow('MediaPipe Hands', image)
       
-      r.sleep()
+      #r.sleep()
       if cv2.waitKey(5) & 0xFF == 27:
         break
     
 if __name__ == '__main__':
     try:
       main()
-    except rospy.ROSInterruptException: 
+    except:# rospy.ROSInterruptException: 
       pass  
