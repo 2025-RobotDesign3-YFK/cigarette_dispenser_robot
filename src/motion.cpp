80b6a5f9f6e018e00f834780293a731a048bd86e
Fujitake <hiroto20060205@email.com>
2025-12-21 15:45:24 +0900

    ライセンス規約に則って文を追加

--- src/motion.cpp
+++ src/motion.cpp
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Keitaro Nakamura,Ryotaro karikomi
+// SPDX-FileCopyrightText: 2025 Hiroto Fujitake, Shogo Yamashita, Reoto Koya
 // SPDX-License-Identifier: Apache 2.0
 #include <cmath>
 
